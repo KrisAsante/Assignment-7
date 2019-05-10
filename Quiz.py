f16bf64358c5ff2c0e2094734bbdc6f218bb83cd
Chris <36240719+KrisAsante@users.noreply.github.com>
2019-05-10 14:27:15 -0400

    Add files via upload

---
+++ Quiz.py
@@ -0,0 +1,88 @@
+# Created by: Chris Asante
+# Created on: 10-May-2019
+# Created for: ICS3U
+# Assignment 7
+# This is program is a pratice quiz for factors
+
+print ("Welcome to Factor Practice. Type the answers as '(number) and (number)'. example: '2 and 4'")
+
+question1 = input("What two numbers multiply to 9 and adds to -6")
+answer1 = ("-3 and -3")
+
+
+if question1 == answer1:
+    print ("Correct")
+elif question1 != answer1:
+    print ("Incorrect! The correct numbers are -3 and -3")
+    
+question2 = input("What two numbers multiply to 4 and adds to 4")
+answer2 = ("2 and 2")
+
+if question2 == answer2:
+    print ("Correct")
+elif question2 != answer2:
+    print ("Incorrect! The correct numbers are 2 and 2")
+    
+question3 = input("What two numbers multiply to -30 and adds to -13")
+answer3 = ("-15 and 2")
+answer3 = ("2 and -15")
+
+if question3 == answer3:
+    print ("Correct")
+elif question3 != answer3:
+    print ("Incorrect! The correct numbers are -15 and 2")
+    
+question4 = input("What two numbers multiply to -84 and adds to 5")
+answer4 = ("12 and -7")
+answer4 = ("-7 and 12")
+if question4 == answer4:
+    print ("Correct")
+elif question4 != answer4:
+    print ("Incorrect! The correct numbers are 12 and -7")
+    
+question5 = input("What two numbers multiply to -24 and adds to -5")
+answer5 = ("3 and -8")
+answer5 = ("-8 and 3")
+
+if question5 == answer5:
+    print ("Correct")
+elif question5 != answer5:
+    print ("Incorrect! The correct numbers are 3 and -8")
+    
+question6 = input("What two numbers multiply to 6 and adds to -5")
+answer6 = ("-3 and -2")
+answer6 = ("-2 and -3")
+
+if question6 == answer6:
+    print ("Correct")
+elif question6 != answer6:
+    print ("Incorrect! The correct numbers are -3 and -2")
+    
+question7 = input("What two numbers multiply to -15 and adds to -14")
+answer7 = ("-15 and 1")
+answer7 = ("1 and -15")
+
+if question7 == answer7:
+    print ("Correct")
+elif question7 != answer7:
+    print ("Incorrect! The correct numbers are -15 and 1")
+    
+question8 = input("What two numbers multiply to -75 and adds to -10")
+answer8 = ("-15 and 5")
+answer8 = ("5 and -15")
+
+if question8 == answer8:
+    print ("Correct")
+elif question8 != answer8:
+    print ("Incorrect! The correct numbers are -15 and 5")
+    
+question9 = input("What two numbers multiply to 18 and adds to -9")
+answer9 = ("-3 and -6")
+answer9 = ("-6 and -3")
+
+if question9 == answer9:
+    print ("Correct")
+elif question9 != answer9:
+    print ("Incorrect! The correct numbers are -3 and -6")
+    
+goodbye = input("Thanks for playing.")
